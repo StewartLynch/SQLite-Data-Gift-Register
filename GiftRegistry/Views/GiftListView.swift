79d7cdda745814367fb03a8e47f58e33ad288c58
Stewart Lynch <slynch@createchsol.com>
2026-03-22 09:17:22 -0700

    Fixed

--- GiftRegistry/Views/GiftListView.swift
+++ GiftRegistry/Views/GiftListView.swift
@@ -51,7 +51,7 @@ class GiftListViewModel {
                             ($0.isPurchased.desc(), $0.name)
                         }
                         .where {
-                            $0.personID == personID
+                            $0.personID.is(personID)
                         }
                         .leftJoin(GiftAsset.all) {
                             $0.id.eq($1.giftID)
@@ -79,8 +79,8 @@ class GiftListViewModel {
                 try Gift
                     .find(gift.id)
                     .update {
-                        $0.isPurchased.toggle()
-                        $0.updatedAt = Date()
+                        $0.isPurchased = #bind(!gift.isPurchased)
+                        $0.updatedAt = #bind(Date())
                     }
                     .execute(db)
             }
