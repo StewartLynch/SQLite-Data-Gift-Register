8219a9a7033ce834b77629354605eef6f3a961e2
Stewart Lynch <slynch@createchsol.com>
2026-03-22 09:03:00 -0700

    Updated PersonForm

--- GiftRegistry/Views/PersonForm.swift
+++ GiftRegistry/Views/PersonForm.swift
@@ -68,7 +68,7 @@ class PersonFormModel {
                             ($0.isPurchased.desc(), $0.name)
                         }
                         .where {
-                            $0.personID == person.id
+                            $0.personID.is(person.id)
                         }
                         .leftJoin(GiftAsset.all) {
                             $0.id.eq($1.giftID)
